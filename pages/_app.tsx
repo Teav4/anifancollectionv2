706e01b77ed73aec8016b19d9844d2a111a721ba
teav4 <teav4@feadorra.com>
2022-04-30 18:03:54 +0700

    feat: add login default state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { AuthContextProvider } from 'modules/auth'
 
-function MyApp({ Component, pageProps }: any) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: any) {
+  return (
+    <AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  )
 }
 
-export default MyApp
+export default App
